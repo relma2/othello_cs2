0227e6dfb8be60d8c036c30c8cbd8d4273dddcad
Amulya Mohan <amohan@flail.cms.caltech.edu>
2016-03-11 21:12:20 -0800

    Message  ...

--- TreeNode.h
+++ TreeNode.h
@@ -3,24 +3,32 @@
 
 #include "common.h"
 #include "board.h"
+#include <vector>
 
 class TreeNode	{
 private:
     Board * myBoard;
     int bscore, wscore;  // score variables
+    int heuristic;  // is heuristic value of this branch of the tree
     bool delete_children;  // Sets whether we delete child nodes in destructor
     Side myside;  // whose turn is it???
-    std::vector<TreeNode *> children 
+    Side playerside; // color player is playing as
+    std::vector<TreeNode *> children;
 
 public:
     TreeNode(); // Constructs with starter board.
-    TreeNode(Board * init_board, Side side); // Constructs with given board and side
+    TreeNode(Board * init_board, Side side, Side player); // Constructs with given board and side
     ~TreeNode(); // Destructor
     void setDeleteChildren(bool b);
     TreeNode * getChild(int idx);
+    void setHeuristic(int h);
+    int getHeuristic();
+    int getScore(Side side);
     
-    void generate_children();  // Most important function, the node makes 
-			       // children based on possible moves
-}
+    void generateChildren();  // Most important function, the node makes 
+			      // children based on possible moves
+    
+    void minimaxCompute(int depth);
+};
 
 #endif
